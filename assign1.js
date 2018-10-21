ed19b697be3ab0cb5fa543f378d504a013b39872
sachinhegde1 <sachin_hegde1@yahoo.com>
2018-10-21 12:23:43 -0500

    For assignment 1

---
+++ assign1.js
@@ -0,0 +1,17 @@
+// assign1
+	sum = 0;
+	for(var i = 3; sum < 1000 ;i++)
+		
+	    {
+			if (i % 3 ==0)
+            {	
+            sum = sum + i;
+			if (sum < 1000)
+			 {
+		    	sum2 = sum;
+			 }
+			}
+		}
+
+console.log(sum2);
+
